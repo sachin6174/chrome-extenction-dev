5e6ed7c60d9ccc4372a871f3de0900cdb80a343a
Sachin Kumar <sachinmehtab@gmail.com>
2023-12-03 16:14:41 +0530

    corrected structure

--- UtilFire/content.js
+++ UtilFire/content.js
@@ -7,6 +7,29 @@ for (var i = 0; i < allLinks.length; i++) {
     // window.open(allLinks[i].href, "_blank");
 }
 
+
+// Wait for the page to fully load
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the element with id "rcnt"
+  var rcntElement = document.getElementById('rcnt');
+
+  if (rcntElement) {
+    // Find all anchor elements within the "rcnt" element
+    var anchorElements = rcntElement.querySelectorAll('a');
+
+    // Add a border to each found anchor element
+    anchorElements.forEach(function (anchor) {
+      anchor.style.border = '1px solid #ff0000'; // You can change the color and size of the border as needed
+    });
+  }
+});
+
+
+
+
+
+
+/***** button *****/
 var button = document.createElement("button");
 
 // Set button properties
@@ -29,3 +52,4 @@ setInterval(function() {
         button.style.transform = "scale(1)";
     }, 500);
 }, 5000);
+
